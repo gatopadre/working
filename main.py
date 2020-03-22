a615428f677e28133d96e1a86c86652e076927e9
SebaZuniga <sebastianzunigasaavedra@gmail.com>
2020-03-22 19:43:17 -0300

    commit inicial

---
+++ main.py
@@ -0,0 +1,164 @@
+#!/usr/bin/python
+import psycopg2
+from config import config
+
+def connect():
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # read connection parameters
+        params = config()
+
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params)
+
+        # create a cursor
+        cur = conn.cursor()
+
+   # execute a statement
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
+
+       # close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+def get_test_data():
+    """ query data from the test table """
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM test ORDER BY id")
+        print("The number of registers: ", cur.rowcount)
+        row = cur.fetchone()
+ 
+        while row is not None:
+            print(row)
+            row = cur.fetchone()
+ 
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+def get_test_data_2():
+    """ query parts from the parts table """
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM test ORDER BY id")
+        rows = cur.fetchall()
+        print("The number of registers: ", cur.rowcount)
+        for row in rows:
+            print(row)
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+def insert_register(text):
+    """ insert a new register into the test table """
+    sql = """INSERT INTO test (text)
+             VALUES(%s) RETURNING id;"""
+    conn = None
+    vendor_id = None
+    try:
+        # read database configuration
+        params = config()
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the INSERT statement
+        cur.execute(sql, (text,))
+        # get the generated id back
+        vendor_id = cur.fetchone()[0]
+        # commit the changes to the database
+        conn.commit()
+        # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+ 
+    return vendor_id
+
+def insert_register_list(resgister_list):
+    """ insert multiple registers into the test table  """
+    sql = "INSERT INTO test(text) VALUES(%s)"
+    conn = None
+    try:
+        # read database configuration
+        params = config()
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the INSERT statement
+        cur.executemany(sql,resgister_list)
+        # commit the changes to the database
+        conn.commit()
+        # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+def delete_register(register_id):
+    """ delete register by register id """
+    conn = None
+    rows_deleted = 0
+    try:
+        # read database configuration
+        params = config()
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the UPDATE  statement
+        cur.execute("DELETE FROM test WHERE id = %s", (register_id,))
+        # get the number of updated rows
+        rows_deleted = cur.rowcount
+        # Commit the changes to the database
+        conn.commit()
+        # Close communication with the PostgreSQL database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+ 
+    return rows_deleted
+
+if __name__ == '__main__':
+    # connect() # conexion
+    get_test_data() # obtener data
+    # insert_register('texto de prueba 6') # insertar un registro
+    insert_register_list([
+        ('texto de prueba 7',),
+        ('texto de prueba 8',)])
+    delete_register(71)
+    get_test_data_2() # obtener data 2
